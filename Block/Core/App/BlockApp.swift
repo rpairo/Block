f494eeabab45e5dd7eb9bffdee88baedca0e842d
Raúl Pera Pairó <raul_pairo@icloud.com>
2022-03-07 02:04:40 +0000

    Dependency injection implementated

--- Block/Core/App/BlockApp.swift
+++ Block/Core/App/BlockApp.swift
@@ -11,7 +11,15 @@ import SwiftUI
 struct BlockApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            EmployeeListView(
+                viewModel: ListViewModel(
+                    fetchEmployeeUseCase: FetchEmployeesUseCase(
+                        repository: EmployeeRepository(
+                            dataSource: EmployeeAPIDataSource()
+                        )
+                    )
+                )
+            )
         }
     }
 }
